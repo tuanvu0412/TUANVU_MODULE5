ea4ad2b398a14332ce142b709ff1b1de1aa4c96a
tuanvu0412 <tuanvu041299@gmail.com>
2023-07-19 11:05:31 +0700

    fixed

--- ss2_reactJS_jsx/bai-tap/src/index.js
+++ ss2_reactJS_jsx/bai-tap/src/index.js
@@ -3,31 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 const students = [
   {
+    id:1,
     company: 'Alfreds Futterkiste',
     contact: 'Maria Anders',
     country: 'Germany'
   },
   {
+    id:2,
     company: 'Centro comercial Moctezuma',
     contact: 'Francisco Chang',
     country: 'Mexico'
   },
   {
+    id:3,
     company: 'Ernst Handel',
     contact: 'Roland Mendel',
     country: 'Austria'
   },
   {
+    id:4,
     company: 'Island Trading',
     contact: 'Helen Bennett',
     country: 'UK'
   },
   {
+    id:5,
     company: 'Laughing Bacchus Winecellars',
     contact: 'Yoshi Tannamuri',
     country: 'Canada'
   },
   {
+    id:6,
     company: 'Magazzini Alimentari Riuniti',
     contact: 'Giovanni Rovelli',
     country: 'Italy'
@@ -37,7 +43,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   students.map(student =>
     (
-   <tr>
+   <tr key={student.id}>
      <td>{student.company}</td>
      <td>{student.contact}</td>
      <td>{student.country}</td>
